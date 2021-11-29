54d2e8de203e3c0f835849da88d51e5085bf9625
angelo-marasa <angelo.marasa@kaleidico.com>
2021-11-29 12:47:59 -0500

    Added https instead of http on css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         @yield('meta')
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
         <title>Laravel</title>
 
